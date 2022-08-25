b99103a310bacf9a1acc2187a8e8826f1eae5994
Mitrich <boikovdmitrii@gmail.com>
2022-08-25 22:20:10 +0300

    delete jbdc implements for IngredientRepository and OrderRepository

--- src/main/java/tacos/data/IngredientRepository.java
+++ src/main/java/tacos/data/IngredientRepository.java
@@ -1,12 +1,7 @@
 package tacos.data;
 
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.repository.Repository;
 import tacos.Ingredient;
 
 public interface IngredientRepository extends Repository<Ingredient, String> {
-    List<Ingredient> findAll();
-    Optional<Ingredient> findById(String id);
-    Ingredient save(Ingredient ingredient);
 }
